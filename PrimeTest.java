a534c122a66724ea64504b244540be2ecc605c7c
Streamwei <weixueren@sina.cn>
2023-06-02 21:01:29 +0800

    Update PrimeTest.java

--- PrimeTest.java
+++ PrimeTest.java
@@ -4,14 +4,5 @@ public class PrimeTest {
 
     public static void main(String[] args) {
     }
-
-    public static boolean isPrime1(int n){
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0){
-                return true;
-            }
-        }
-        return false;
-    }
-
+    
 }
